530df0e7585c1c9337b78f53b70ba557bbb8373b
Jeff Miller <jmiller@gmail.com>
2012-04-20 16:45:38 -0700

    Fix broken version dependency.

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@ import os
 import sys
 from distutils.core import setup
 
-# Don't import punchfork module here, since deps may not be installed
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'punchfork'))
-import version
-
 path, script = os.path.split(sys.argv[0])
 os.chdir(os.path.abspath(path))
 
 setup(name='punchfork',
-      version=version.VERSION,
+      version='0.1',
       description='Punchfork API Python bindings',
       author='Punchfork, Inc.',
       author_email='api@punchfork.com',
